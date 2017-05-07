a8aa5c1c63e1fd85ebfc410db3f018f2d0e1daad
Drag <drag.faq@gmail.com>
2017-05-08 01:02:44 +0300

    First version of all task

---
+++ src/main/java/com/drag/linkedlist/Item.java
@@ -0,0 +1,31 @@
+package main.java.com.drag.linkedlist;
+
+public class Item {
+    private int value;
+    private Item next;
+
+    public Item(int value, Item next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public Item(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Item getNext() {
+        return next;
+    }
+
+    public void setNext(Item next) {
+        this.next = next;
+    }
+}
---
+++ src/main/java/com/drag/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,96 @@
+package main.java.com.drag.linkedlist;
+
+public class SinglyLinkedList {
+    private Item first;
+    private Item last;
+    private int count;
+
+    public SinglyLinkedList() {
+        count = 0;
+    }
+
+    public Item getFirst() {
+        return first;
+    }
+
+    public Item getLast() {
+        return last;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Item append(int value) {
+        Item item = new Item(value);
+        if (count == 0) {
+            first = item;
+            last = item;
+        } else {
+            last.setNext(item);
+            last = item;
+        }
+        count ++;
+
+        return item;
+    }
+
+    public void removeLast() {
+        if (count == 0)
+            return;
+
+        if (count == 1) {
+            first = null;
+            last = null;
+        } else {
+            Item item = first;
+
+            while ((item = item.getNext()) != last) {
+            }
+
+            item.setNext(null);
+            last = item;
+        }
+
+        count --;
+    }
+
+    public void removeAllGreaterThan(int value) {
+        if (count == 0)
+            return;
+
+        // Need to receive first element less then target value
+        boolean isNeedAnotherOneCycle = true;
+        while (isNeedAnotherOneCycle) {
+            isNeedAnotherOneCycle = false;
+            if (first.getValue() > value) {
+                if (first == last) {
+                    removeLast();
+                    return;
+                } else {
+                    first = first.getNext();
+                    count --;
+                    isNeedAnotherOneCycle = true;
+                }
+            }
+        }
+
+        // bypass all "bad" items except the last one
+        if (count > 1) {
+            Item item = first;
+
+            while (item.getNext() != last) {
+                if (item.getNext().getValue() > value) {
+                    item.setNext(item.getNext().getNext());
+                    count--;
+                } else {
+                    item = item.getNext();
+                }
+            }
+        }
+
+        // check the last one
+        if (last.getValue() > value)
+            removeLast();
+    }
+}
